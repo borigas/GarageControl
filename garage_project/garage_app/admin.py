b5c04df2128652f095a75d72461212cc4c6ab765
Ben Origas <borigas@gmail.com>
2013-07-10 22:55:45 +0000

    Added new status to the admin page

--- garage_project/garage_app/admin.py
+++ garage_project/garage_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from garage_app.models import Door, Status
 
-admin.site.register(Door)
+
+class DoorAdmin(admin.ModelAdmin):
+	list_display = ['name']
+	actions = ['update_statuses']
+
+	def update_statuses(self, request, queryset):
+		for door in queryset:
+			door.update_status()
+
+admin.site.register(Door, DoorAdmin)
+
 admin.site.register(Status)
--- garage_project/garage_app/models.py
+++ garage_project/garage_app/models.py
@@ -5,10 +5,23 @@ from django.db import models
 class Door(models.Model):
 	name = models.CharField(max_length=200)
 
+	def update_status(self):
+		status = Status(door = self, distance = 1.5, isDoorUp = False, isCarPresent = True)
+		status.save()
+		return status
+	
+	def __unicode__(self):
+		return self.name
+
 class Status(models.Model):
+	class Meta:
+		verbose_name_plural = 'Statuses'
+
 	door = models.ForeignKey(Door)
 	distance = models.DecimalField(max_digits=10, decimal_places=4)
 	isDoorUp = models.BooleanField(default=True)
 	isCarPresent = models.BooleanField(default=False)
 	statusDate = models.DateField(auto_now_add=True)
 
+	def __unicode__(self):
+		return str(self.distance)
